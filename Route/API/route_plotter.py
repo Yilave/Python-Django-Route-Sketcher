285eff927af16bd5f934c61a961063dd2120c7eb
Yilave Isaac <yilavesughnenter97@gmail.com>
2025-03-10 07:57:09 +0100

    Almost done and waiting for test

--- Route/API/route_plotter.py
+++ Route/API/route_plotter.py
@@ -1,102 +1,147 @@
 # Import the needed Liabraries
-import logging
-import json
-import polyline
-import folium
-from folium.plugins import MeasureControl
-import geocoder
-
-from functools import lru_cache
+import os
 import numpy as np
 import pandas as pd
-
-
-logger = logging.getLogger(__name__)
-DEBUG = True
-
+from geopy.geocoders import Nominatim
+from geopy.distance import geodesic
 import pip._vendor.requests as requests
 import xml.etree.ElementTree as ET
 from tqdm import tqdm
+from multiprocessing import Process
+import time
+
 
 # Plotting
 import plotly.express as px
+
+import logging
+import json
+from functools import lru_cache
+
+FILE = os.path.abspath(__file__)
+BASE_DIR = os.path.dirname(FILE)
+
+logger = logging.getLogger(__name__)
+DEBUG = True
 
 @lru_cache(maxsize=None)
 def geocode(location):
     return _geocode(location)
 
 def _geocode(location):
-    import geocoder
-    g = geocoder.osm(location)
-    return g.latlng
+    loc = Nominatim(user_agent="Geopy Library")
+    getLoc = loc.geocode(location)
+    loc = (getLoc.longitude, getLoc.latitude) 
+    return loc
+
 
 @lru_cache(maxsize=None)
-def get_route(olat, olng, dlat, dlng):
-    response = _get_route(olat, olng, dlat, dlng)
+def distance(origin, dest):
+    response = _distance(origin, dest)
     return response
 
-def _get_route(olat, olng, dlat, dlng):
-    url = f'http://router.project-osrm.org/route/v1/driving/{olng},{olat};{dlng},{dlat}?alternatives=false&steps=false'
-    # logger.debug(url)
-    response = None
+def _distance(origin, dest):
+    distance_ = geodesic((geocode(origin)[1], geocode(origin)[0]), (geocode(dest)[1], geocode(dest)[0])).miles
+    return distance_
 
-    try:
-        logger.debug(f'====== OSRM: {url}')
-        response = requests.get(url, verify=False)
-    except Exception as ex:
-        raise
-    
-    # logger.debug(response.text)
-    if response and response.text:
-        response_dict = json.loads(response.text)
-        #possible = pd.DataFrame([{'Distance': (route['distance'] / 1000) *  0.621371 , route['weight_name']: route['weight']} for route in response_dict['routes']])
-        return response_dict
-    else:
-        return None
+@lru_cache(maxsize=None)
+def get_route(origin, destination):
 
-route_nodes = routejson['routes'][0]['legs'][0]['annotation']['nodes']
-
-### keeping every third element in the node list to optimise time
-route_list = []
-for i in range(0, len(route_nodes)):
-    if i % 3==1:
-        route_list.append(route_nodes[i])
-
-coordinates = []
-
-for node in tqdm(route_list):
-    try:
-        url = 'https://api.openstreetmap.org/api/0.6/node/' + str(node)
-        r = requests.get(url, headers = headers)
-        myroot = ET.fromstring(r.text)
-        print("###############")
-        print(myroot)
-        for child in myroot:
-            lat, long = child.attrib['lat'], child.attrib['lon']
-        coordinates.append((lat, long))
-    except:
-        continue
-print(coordinates[:10])
+    start_time = time.time()
 
 
-df_out = pd.DataFrame({'Node': np.arange(len(coordinates))})
-df_out['coordinates'] = coordinates
-df_out[['lat', 'long']] = pd.DataFrame(df_out['coordinates'].tolist(),)
+    #source = (-83.920699, 35.96061) # Knoxville 
+    #dest  = (-73.973846, 40.71742)  # New York City
 
-# Converting Latitude and Longitude into float
-df_out['lat'] = df_out['lat'].astype(float)
-df_out['long'] = df_out['long'].astype(float)
-
-# Plotting the coordinates on map
-color_scale = [(0, 'red'), (1,'green')]
-fig = px.scatter_mapbox(df_out, 
-                        lat="lat", 
-                        lon="long", 
-                        zoom=8, 
-                        height=600,
-                        width=900)
+    start = "{},{}".format(geocode(origin)[0], geocode(origin)[1])
+    end = "{},{}".format(geocode(destination)[0], geocode(destination)[1])
+    # Service - 'route', mode of transportation - 'driving', without alternatives
+    url = 'http://router.project-osrm.org/route/v1/driving/{};{}?alternatives=false&annotations=nodes'.format(start, end)
 
 
-fig.update_layout(mapbox_style="open-street-map")
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-fig.show()
+    headers = { 'Content-type': 'application/json'}
+    r = requests.get(url, headers = headers)
+    print("Calling API ...:", r.status_code) # Status Code 200 is success
+
+
+    routejson = r.json()
+    print(routejson)
+    route_nodes = routejson['routes'][0]['legs'][0]['annotation']['nodes']
+
+    # print("Nodes number before: ", len(route_nodes))
+
+    # print(route_nodes)
+    distance_traveled =  distance(origin, destination)
+
+    ### keeping every third element in the node list to optimise time
+    # Note: the distance between each node on the rout is 100 metres apart
+    route_list = []
+    for i in range(0, len(route_nodes)):
+        if i % 300 == 1:
+            route_list.append(route_nodes[i])
+
+    coordinates = []
+
+    for node in tqdm(route_list):
+        try:
+            url = 'https://api.openstreetmap.org/api/0.6/node/' + str(node)
+            r = requests.get(url, headers = headers)
+            myroot = ET.fromstring(r.text)
+            for child in myroot:
+                lat, long = child.attrib['lat'], child.attrib['lon']
+            coordinates.append((lat, long))
+        except:
+            continue
+    print(r.text)
+
+    df_out = pd.DataFrame({'Node': np.arange(len(coordinates))})
+    # df_out['nodes'] = final_nodes
+    df_out['coordinates'] = coordinates
+    df_out[['lat', 'long']] = pd.DataFrame(df_out['coordinates'].tolist())
+
+    # Converting Latitude and Longitude into float
+    df_out['lat'] = df_out['lat'].astype(float)
+    df_out['long'] = df_out['long'].astype(float)
+
+    df_lat = df_out['lat'].to_list()
+    df_long = df_out['long'].to_list()
+    lon_lat = zip(df_long, df_lat)
+
+
+    # Get the average price data in the csv and compute the 
+    # price_data = pd.read_csv('C:/Users/ISAAC/Desktop/ASSIGNMENT/Django Developer/Route/fuel-prices-for-be-assessment.csv')
+    price_data = pd.read_csv(BASE_DIR + "/fuel-prices-for-be-assessment.csv")
+
+    average_price = price_data['Retail Price'].mean() 
+    # print(f"Average Price === {average_price}")
+    gallons = distance_traveled / 10
+
+    total_fuel_cost = gallons * average_price
+        
+    data = {
+        "data": df_out, 
+        "lon_lat": lon_lat,
+        "lat": df_lat,
+        "lon": df_long,
+        "distance": f"{abs(distance_traveled)} Miles", 
+        "gallons": gallons, 
+        "average_price": abs(average_price), 
+        "total_cost": f"${abs(total_fuel_cost)}", 
+        # "nodes": final_nodes, 
+        "length": len(route_list)
+        }
+    # end_time = time.time()
+    # execution_time = end_time - start_time
+    # print(execution_time)
+
+    return data
+
+if __name__ == '__main__':
+    p = Process(target=get_route, args=('bob',))
+    p.start()
+    p.join()
+
+   
+
+# get_route('Brooklyn, NY', 'Philadelphia, Pennsylvania')
+
---
+++ Route/core/views.py
@@ -0,0 +1,71 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.http import JsonResponse
+import json
+from API.route_plotter import get_route
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.offline import plot
+
+# Create your views here.
+def index(request):
+    context = {}
+    template = "index.html"
+    if request.method == "POST":
+        try:
+            origin = request.POST['origin']
+            destination = request.POST['dest']
+            data = get_route(origin, destination)
+            # nodes = data['']
+            df = data['data']
+            longitude = data['lon']
+            latitude = data['lat']
+            
+                # Plotting the coordinates on map
+            fig = go.Figure()
+            color_scale = [(0, 'red'), (1,'green')]
+            fig = px.line_mapbox(df, 
+                        lat="lat", 
+                        lon="long", 
+                        zoom=8, 
+                        height=500,
+                    width=900)
+            
+
+            fig.update_layout(
+                mapbox_style="open-street-map",
+                )
+            fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+            
+            for lon, lat in data['lon_lat']:
+                fig.add_trace(go.Scattermapbox(mode="markers",
+                    lon=[longitude[-1],lon],
+                    lat=[latitude[-1], lat],
+                    marker={
+                        'size': 10,
+                        "color": "green"
+                    }
+                ))
+            map = plot(fig, output_type="div")
+            # return map
+            context['map'] = map
+            context['distance'] = data['distance']
+            context['gallons'] = data['gallons']
+            context['average_price'] = data['average_price']
+            context['total_cost'] = data['total_cost']
+            context['origin'] = origin
+            context['destination'] = destination
+
+            return render(request, template, context)
+        except:
+            messages.error(request, "Coult not find places on Map. Please make sure you enter a valid location")
+            return redirect('/')
+    return render(request, template, context)
+
+
+def get_route_api(request):
+    post_body = json.loades(request.body)
+    origin = post_body['origin']
+    destination = post_body['destination']
+    data = get_route(origin, destination)
+    return JsonResponse(data)
